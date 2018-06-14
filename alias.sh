7b7046346a937b8b80c78a31c8b75fa04724560e
Joseph Burnett <josephburnett79@gmail.com>
2018-06-14 12:21:30 -0400

    Update alias.sh.

--- alias.sh
+++ alias.sh
@@ -4,9 +4,8 @@ alias fm='xdg-open https://fastmail.com >/dev/null &'
 alias gd='git diff'
 alias gs='git status'
 alias kickshell='exec bash -l'
-alias kp='xdg-open https://keep.google.com >/dev/null &'
-alias lx='lynx -use_mouse'
 alias op='xdg-open'
 alias up="upower -i /org/freedesktop/UPower/devices/battery_BAT1 | awk '/percentage/ { print \$2 }'"
 alias gotest="while true ; do inotifywait -qq -e close_write -r . 2>/dev/null ; go test *.go ; done"
+alias pd='pushd'
 
