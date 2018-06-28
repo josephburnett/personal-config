bd677e991746348038fa0235d9536e6145f79ebf
Joseph Burnett <josephburnett79@gmail.com>
2018-06-28 12:21:31 -0400

    Git branch alias.

--- alias.sh
+++ alias.sh
@@ -3,6 +3,7 @@ alias ff='firefox'
 alias fm='xdg-open https://fastmail.com >/dev/null &'
 alias gd='git diff'
 alias gs='git status'
+alias gb="git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
 alias kickshell='exec bash -l'
 alias op='xdg-open'
 alias up="upower -i /org/freedesktop/UPower/devices/battery_BAT1 | awk '/percentage/ { print \$2 }'"
