f5260f878b17151f3abf4d79025f9f5a1db081bb
Joseph Burnett <josephburnett@google.com>
2022-04-26 11:17:40 +0200

    Change kubectl alias to k.

--- alias.sh
+++ alias.sh
@@ -3,11 +3,7 @@ alias gs='git status'
 # https://stackoverflow.com/questions/5188320/how-can-i-get-a-list-of-git-branches-ordered-by-most-recent-commit
 alias gb="git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
 alias kickshell='exec bash -l'
-alias kk='kubectl'
-alias kkn='kubectl -n knative-serving'
-alias kkc='kubectl config current-context'
-alias koa='ko apply'
-alias kod='ko delete  --ignore-not-found'
+alias k='kubectl'
 alias when="ts '[%Y-%m-%d %H:%M:%.S]'"
 alias gotest='while true; do inotifywait -e close_write -r . 2>/dev/null ; clear ; go test ./... ; done'
 alias org='emacs ~/org/log.org'
