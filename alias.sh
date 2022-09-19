f6e6a1f68432cb8efcea57c972022ce574ae30ce
Joseph Burnett <josephburnett79@gmail.com>
2022-09-19 11:04:49 -0700

    Add gt alias for GitLab todos.

--- alias.sh
+++ alias.sh
@@ -2,6 +2,7 @@ alias gd='git diff'
 alias gs='git status'
 # https://stackoverflow.com/questions/5188320/how-can-i-get-a-list-of-git-branches-ordered-by-most-recent-commit
 alias gb="git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
+alias gt="glab api https://gitlab.com/api/v4/todos | jq  '.[] | { target_url, body }'"
 alias kickshell='exec bash -l'
 alias k='kubectl'
 alias when="ts '[%Y-%m-%d %H:%M:%.S]'"
