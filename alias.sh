371946a37b812cfcff553c1773a681e78a3ceae3
Joseph Burnett <josephburnett79@gmail.com>
2022-09-30 08:19:12 -0700

    Check origin with git branch.

--- alias.sh
+++ alias.sh
@@ -1,7 +1,7 @@
 alias gd='git diff'
 alias gs='git status'
 # https://stackoverflow.com/questions/5188320/how-can-i-get-a-list-of-git-branches-ordered-by-most-recent-commit
-alias gb="git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'"
+alias gb="git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))' ; git remote get-url origin"
 alias gt="glab api https://gitlab.com/api/v4/todos | jq  '.[] | { target_url, body }'"
 alias kickshell='exec bash -l'
 alias k='kubectl'
